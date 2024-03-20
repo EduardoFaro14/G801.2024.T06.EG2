32e3b5a6206d1c81433420e14521ca5ad27d7c3f
100475359 <100475359@alumnos.uc3m.es>
2024-03-20 17:49:31 +0100

    funcion room_reservation + intento de meterlo en un archivo con funcion guardar_reserva_en_archivo

--- src/main/python/uc3m_travel/hotel_manager.py
+++ src/main/python/uc3m_travel/hotel_manager.py
@@ -1,8 +1,12 @@
 """Modulo hotelManager"""
 import json
+import os
 from luhn import verify
 from .hotel_management_exception import hotelManagementException
 from .hotel_reservation import hotelReservation
+from stdnum import es
+from datetime import datetime
+
 class hotelManager:
     """ Esta clase comprueba que la tarjeta sea válida y lee los datos de json"""
     def __init__(self):
@@ -38,6 +42,43 @@ class hotelManager:
         # Close the file
         return req
 
+    def validar_formato_fecha(self, cadena):
+        try:
+            # Intentamos analizar la cadena como una fecha en el formato especificado
+            datetime.strptime(cadena, '%d/%m/%Y')
+            return True
+        except ValueError:
+            return False
+
+    def guardar_reserva_en_archivo(self, reserva):
+        # Nombre del archivo donde se guardarán las reservas
+        nombre_archivo = "reservas.json"
+
+        # Si el archivo no existe, crearlo e inicializarlo con una lista vacía
+        if not os.path.exists(nombre_archivo):
+            with open(nombre_archivo, 'w') as f:
+                json.dump([], f)
+
+        # Leer las reservas existentes desde el archivo
+        with open(nombre_archivo, 'r') as f:
+            reservas = json.load(f)
+
+        # Agregar la nueva reserva al archivo de reservas
+        reserva_info = {
+            "localizer": reserva.localizer,
+            "data": str(reserva)
+        }
+        reservas.append(reserva_info)
+
+        # Guardar las reservas actualizadas en el archivo
+        with open(nombre_archivo, 'w') as f:
+            json.dump(reservas, f, indent=4)
 
     def room_reservation(self, creditCardNumber, idCard, nameSurname, phoneNumber, roomType, arrival, numDays):
-
+        if (self.validatecreditcard(creditCardNumber) and es.nif.isvalid(idCard) and 10 <= nameSurname <= 50 and len(nameSurname.split()) >= 2 and len(phoneNumber) == 9 and (roomType == "single" or roomType == "double" or roomType == "suite") and self.validar_formato_fecha(arrival) and 1 <= numDays <= 10):
+            localizador = hotelReservation(idCard, creditCardNumber, nameSurname, phoneNumber, roomType, numDays)
+            self.guardar_reserva_en_archivo(localizador)
+            return localizador
+        else:
+            print("Error, algo falla")
+            raise hotelManagementException("Error en la reserva")
