05357b0c78f9d118c649d18440af613ced51dd37
100475359 <100475359@alumnos.uc3m.es>
2024-04-02 13:25:57 +0200

    Errores pylint pocos

--- src/main/python/uc3m_travel/hotel_manager.py
+++ src/main/python/uc3m_travel/hotel_manager.py
@@ -10,16 +10,19 @@ from stdnum.es import nif
 from datetime import datetime
 from src.main.python.uc3m_travel.hotel_stay import hotelStay
 
+
 class hotelManager:
     """ Esta clase comprueba que la tarjeta sea válida y lee los datos de json"""
+
     def __init__(self):
         pass
 
-    def validatecreditcard( self, x):
+    def validatecreditcard(self, x):
         """valida la tarjeta"""
         return verify(x)
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
+
     def readdatafrom_json(self, fi):
         """leer de JSON"""
         try:
@@ -33,24 +36,27 @@ class hotelManager:
         return data
 
     def validar_formato_fecha(self, cadena):
-        if len(cadena) == 10 and cadena[2] == '/' and cadena[5] == '/' and cadena[0].isdigit() and cadena[1].isdigit() and cadena[3].isdigit() and cadena[4].isdigit() and cadena[6:].isdigit():
+        if len(cadena) == 10 and cadena[2] == '/' and cadena[5] == '/' and cadena[0].isdigit() and cadena[
+            1].isdigit() and cadena[3].isdigit() and cadena[4].isdigit() and cadena[6:].isdigit():
             return True
         else:
             return False
+
     def guardar_reserva2_en_archivo(self, localizador, idCard, roomType, arrival, departure, roomKey):
         # Nombre del archivo donde se guardarán las reservas
-        nombreArchivo = r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri\EG2\src\main\python\json_files\reservas2.json"
+        nombreArchivo = (r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri"
+                         r"\EG2\src\main\python\json_files\reservas2.json")
 
-        #arrival = str(arrival[0].isoformat())
-        #departure = departure[0].isoformat()
-        arrival_str = arrival.strftime("%Y-%m-%d")
-        departure_str = departure.strftime("%Y-%m-%d")
+        # arrival = str(arrival[0].isoformat())
+        # departure = departure[0].isoformat()
+        arrivalstr = arrival.strftime("%Y-%m-%d")
+        departurestr = departure.strftime("%Y-%m-%d")
         datosReservaFinal = {
             "Localizer": localizador,
             "IdCard": idCard,
             "RoomType": roomType,
-            "Arrival": arrival_str,
-            "Departure": departure_str,
+            "Arrival": arrivalstr,
+            "Departure": departurestr,
             "roomKey": roomKey
         }
 
@@ -68,9 +74,11 @@ class hotelManager:
             with open(nombreArchivo, 'w') as f:
                 json.dump(contenido, f, indent=4)
 
-    def guardar_reserva_en_archivo(self, localizador, idCard, creditCardNumber, arrival, nameSurname, phoneNumber, roomType, numDays):
+    def guardar_reserva_en_archivo(self, localizador, idCard, creditCardNumber, arrival, nameSurname, phoneNumber,
+                                   roomType, numDays):
         # Nombre del archivo donde se guardarán las reservas
-        nombreArchivo = r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri\EG2\src\main\python\json_files\reservas.json"
+        nombreArchivo = (r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri"
+                         r"\EG2\src\main\python\json_files\reservas.json")
 
         nuevoRegistro = {
             "Localizer": localizador,
@@ -97,7 +105,8 @@ class hotelManager:
                 json.dump(contenido, f, indent=4)
 
     def insertarDatos(self, fechaActual, roomKey):
-        nombreArchivo = r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri\EG2\src\main\python\json_files\reservas3.json"
+        nombreArchivo = (r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri"
+                         r"\EG2\src\main\python\json_files\reservas3.json")
         nuevoRegistro = {
             "Fecha de Salida": fechaActual,
             "roomKey": roomKey
@@ -143,7 +152,7 @@ class hotelManager:
         for i in range(1, len(nameSurname)):
             if nameSurname[i] == ' ' and nameSurname[i - 1] == ' ':
                 raise hotelManagementException("Nombre y apellido erroneo, dos espacios seguidos")
-        if nameSurname[0]==' ':
+        if nameSurname[0] == ' ':
             raise hotelManagementException("Nombre y apellido erroneo, primer caracter es un espacio")
         if nameSurname.endswith(' '):
             raise hotelManagementException("Nombre y apellido erroneo, último caracter es un espacio")
@@ -189,9 +198,9 @@ class hotelManager:
 
         reservaHotel = hotelReservation(idCard, creditCardNumber, arrival, nameSurname, phoneNumber, roomType, numDays)
         localizador = reservaHotel.localizer
-        self.guardar_reserva_en_archivo(localizador, idCard, creditCardNumber, arrival, nameSurname, phoneNumber, roomType, numDays)
+        self.guardar_reserva_en_archivo(localizador, idCard, creditCardNumber, arrival, nameSurname
+                                        , phoneNumber, roomType, numDays)
         return localizador
-    
 
     def guest_arrival(self, input_file):
         try:
@@ -217,16 +226,16 @@ class hotelManager:
 
         # Verificar si hay una clave vacía en el JSON
         for key, value in datosReservaf2.items():
-            if not value and key==("Localizer"):
+            if not value and key == ("Localizer"):
                 raise hotelManagementException("Error, el valor asociado a la clave Localizer está vacío")
-            if not value and key==("IdCard"):
+            if not value and key == ("IdCard"):
                 raise hotelManagementException("Error, el valor asociado a la clave IdCard está vacío")
 
         # Verificar si el JSON tiene el Localizer y el IdCard
-        if "Localizer" not in datosReservaf2 or "IdCard" not in datosReservaf2: #si esto falla yo pondría datosReservaf2[0]
+        if "Localizer" not in datosReservaf2 or "IdCard" not in datosReservaf2:
             raise hotelManagementException("El archivo reservasf2 tiene un fallo de escritura en alguna de las claves")
 
-        #Verificar si el valor de las claves está correctamente escrito
+        # Verificar si el valor de las claves está correctamente escrito
         if len(datosReservaf2["Localizer"]) > 32:
             raise hotelManagementException("Localizer contiene más de 32 caracteres")
         if len(datosReservaf2["Localizer"]) < 32:
@@ -234,7 +243,7 @@ class hotelManager:
         if not datosReservaf2["Localizer"].isalnum():
             raise hotelManagementException("Localizer no contiene solo números o solo letras")
 
-        #Verificar que el IdCard tiene 8 números y 1 letra
+        # Verificar que el IdCard tiene 8 números y 1 letra
         if len(datosReservaf2["IdCard"]) > 9:
             raise hotelManagementException("IdCard contiene más de 9 caracteres")
         if len(datosReservaf2["IdCard"]) < 9:
@@ -242,14 +251,14 @@ class hotelManager:
         if not nif.is_valid(datosReservaf2["IdCard"]):
             raise hotelManagementException("El formato de IdCard no cumple el formato 8 dígitos y 1 letra")
 
-
         localizer = datosReservaf2["Localizer"]
-        idCard = datosReservaf2["IdCard"]
+        id_card = datosReservaf2["IdCard"]
 
         # verificar que el localizador fue almacenado en el fichero de reservas y
         # que el localizador coincide con los datos que estaban en el fichero
         try:
-            with open(r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri\EG2\src\main\python\json_files\reservas.json", 'r') as f:
+            with open(r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri"
+                      r"\EG2\src\main\python\json_files\reservas.json", 'r') as f:
                 datosReservaf1 = json.load(f)
         except FileNotFoundError:
             datosReservaf1 = {}
@@ -269,25 +278,27 @@ class hotelManager:
         localizer2 = reserva2["Localizer"]
         idCard2 = reserva2["IdCard"]
         arrivalReserva = reserva2["Arrival"]
-        #arrivalReserva_datetime = datetime.strptime(arrivalReserva, "%d/%m/%Y")
+        # arrivalReserva_datetime = datetime.strptime(arrivalReserva, "%d/%m/%Y")
         numDays = reserva2["NumDays"]
-        roomType = reserva2["RoomType"]
+        room_type = reserva2["RoomType"]
 
         if localizer != localizer2:
-            raise hotelManagementException("El localizador de reservaf2 no se corresponde con el localizador en el archivo de reservaf1")
+            raise hotelManagementException("El localizador de reservaf2 no se corresponde con "
+                                           "el localizador en el archivo de reservaf1")
 
-        if idCard != idCard2:
+        if id_card != idCard2:
             raise hotelManagementException("El DNI de reservaf2 no corresponde con el DNI de reservaf1")
 
-        hotel = hotelStay(idCard2, localizer2, numDays, roomType)
+        hotel = hotelStay(idCard2, localizer2, numDays, room_type)
         arrival = hotel.arrival
         departure = hotel.departure
-        roomKey = hotel.room_key
+        room_key = hotel.room_key
         dd, mm, yyyy = map(int, arrivalReserva.split('/'))
         if dd != arrival.day or mm != arrival.month or yyyy != arrival.year:
-            raise hotelManagementException("La fecha de llegada de reservaf2 no coincide con la fecha de llegada en reservaf1")
-        self.guardar_reserva2_en_archivo(localizer2, idCard2, roomType, arrival, departure, roomKey)
-        return roomKey
+            raise hotelManagementException("La fecha de llegada de reservaf2 no "
+                                           "coincide con la fecha de llegada en reservaf1")
+        self.guardar_reserva2_en_archivo(localizer2, idCard2, room_type, arrival, departure, room_key)
+        return room_key
 
     def guest_checkout(self, roomKey):
         # comprobar q roomKey sea hexadecimal de 64 caracteres
@@ -297,7 +308,8 @@ class hotelManager:
             raise hotelManagementException("roomKey tiene más de 64 caracteres")
         if not re.match("^[a-fA-F0-9]{64}$", roomKey):
             raise hotelManagementException("roomKey no es hexadecimal de 64 caracteres")
-        nombreArchivo = (r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri\EG2\src\main\python\json_files\reservas2.json")
+        nombreArchivo = (r"C:\Users\eduardo faro jr\OneDrive\Documentos\3 curso 2 cuatri"
+                         r"\EG2\src\main\python\json_files\reservas2.json")
         datosReservaf1 = self.readdatafrom_json(nombreArchivo)
 
         if not datosReservaf1:
@@ -305,7 +317,7 @@ class hotelManager:
 
         hoy = datetime.now().date().strftime("%Y-%m-%d")
         existe = 0
-        #posicion, contador = 0, 0
+        # posicion, contador = 0, 0
         if not isinstance(datosReservaf1, list):
             datosReservaf1 = [datosReservaf1]
         for a in datosReservaf1:
@@ -315,10 +327,8 @@ class hotelManager:
                     raise hotelManagementException("La fecha de salida no es hoy")
                 fechaSalida = a["Departure"]
 
-
         if existe == 0:
             raise hotelManagementException("No existe ninguna roomKey igual a la dada")
 
         self.insertarDatos(fechaSalida, roomKey)
         return True
-
